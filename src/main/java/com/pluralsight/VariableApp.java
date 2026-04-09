70d680fe71244cb6e2d3e308d97e23b862dcaf17
thetimstarcev <132023910+thetimstarcev@users.noreply.github.com>
2026-04-09 13:13:19 -0400

    Completed Steps 1 and 2

---
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -0,0 +1,33 @@
+package com.pluralsight;
+
+public class VariableApp {
+    public static void main(String[] args) {
+        String favoriteColor = "blue"; //Favorite color
+        System.out.println("My favorite color is " + favoriteColor);
+
+        int year = 2026; // Started Year
+        System.out.println("Year I started this class is " + year);
+
+        char middleInitial = 'N'; // Middle Initial
+        System.out.println("My middle initial is " + middleInitial);
+
+        boolean pets = false; // Having pets
+        System.out.println("Do you have a pet? " + pets);
+
+        String niceMessage = "Enjoy the rest of your day!"; //Nice message
+        System.out.println("Message: " + niceMessage);
+
+        int daysWeek = 7; //Days in a week
+        System.out.println("Number of days in a week: " + daysWeek);
+
+        double coffeePrice = 7.99; // The price of coffee
+        System.out.println("The price of coffee is " + coffeePrice);
+
+        char letter = 'S'; // My favorite letter
+        System.out.println("My favorite letter is " + letter);
+
+        boolean isRaining = false; // Raining weather
+        System.out.println("Is it raining today? " + isRaining);
+
+    }
+}
